dfd20b45d564b393a6d7ed12323433b11b6018ec
Nicolas L <nicolas@beroot.org>
2016-07-28 21:34:25 +0200

    blackduck sav

---
+++ blackduck.sh
@@ -0,0 +1,2 @@
+./rsync_tmbackup.sh /home/nicolas/Images/ nicolas@nas:/media/nas/backup/blackduck/Images
+./rsync_tmbackup.sh /home/nicolas/Documents/ nicolas@nas:/media/nas/backup/blackduck/Documents
